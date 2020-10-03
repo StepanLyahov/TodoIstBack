5739906900fe4d60c92e86df004b810725bb76d4
stepan <lyakhov.sr@reliab.tech>
2020-10-03 22:33:28 +0300

    create project

---
+++ MyTodoIstBack/src/main/java/com/lyahov/stepa/MyTodoIstBack/MyTodoIstBackApplication.java
@@ -0,0 +1,13 @@
+package com.lyahov.stepa.MyTodoIstBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoIstBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodoIstBackApplication.class, args);
+	}
+
+}
---
+++ MyTodoIstBack/src/test/java/com/lyahov/stepa/MyTodoIstBack/MyTodoIstBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyahov.stepa.MyTodoIstBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoIstBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
