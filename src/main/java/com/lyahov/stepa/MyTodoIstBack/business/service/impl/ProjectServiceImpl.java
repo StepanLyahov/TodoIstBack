9184a38e3e35a8f40a6cb06f40da0b498e0468c0
stepan <lyakhov.sr@reliab.tech>
2020-10-04 20:02:02 +0300

    add logic to ProjectService

--- src/main/java/com/lyahov/stepa/MyTodoIstBack/business/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/lyahov/stepa/MyTodoIstBack/business/service/impl/ProjectServiceImpl.java
@@ -1,43 +1,108 @@
 package com.lyahov.stepa.MyTodoIstBack.business.service.impl;
 
+import com.lyahov.stepa.MyTodoIstBack.business.dao.ProjectRepository;
+import com.lyahov.stepa.MyTodoIstBack.business.dao.TaskRepository;
+import com.lyahov.stepa.MyTodoIstBack.business.entity.ProjectEntity;
+import com.lyahov.stepa.MyTodoIstBack.business.entity.TaskEntity;
 import com.lyahov.stepa.MyTodoIstBack.business.service.ProjectService;
+import com.lyahov.stepa.MyTodoIstBack.utils.mappers.ProjectMapper;
 import com.lyahov.stepa.MyTodoIstBack.web.dto.Project;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class ProjectServiceImpl implements ProjectService  {
+
+    private final ProjectRepository projectRepository;
+    private final TaskRepository taskRepository;
+    private final ProjectMapper projectMapper;
+
     @Override
     public void createProject(Project project) {
+        if (project == null) {
+            log.info("project is void");
+            return;
+        }
         log.info("create new project : " + project.getTitle());
+        projectRepository.save(projectMapper.toEntity(project));
     }
 
     @Override
     public void deleteProject(Long id) {
         log.info("del project by id : " + id);
+        projectRepository.deleteById(id);
     }
 
     @Override
     public void updateProject(Project project) {
+        if (project == null || project.getId() == null) {
+            log.info("project is void");
+            return;
+        }
         log.info("update project: " + project.getId());
+        projectRepository.save(projectMapper.toEntity(project));
     }
 
     @Override
     public List<Project> getAllProject() {
         log.info("get all project");
-        return null;
+        return projectRepository.findAll()
+                .stream().map(projectMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
+    @SneakyThrows
     public void addTaskToProject(Long projectId, Long taskId) {
         log.info("add task: " + taskId + "to project: " + projectId);
+
+        ProjectEntity projectEntity = projectRepository.getOne(projectId);
+        if (projectEntity == null) {
+            log.info("There is no such project");
+            throw new Exception("Такого проекта не существует");
+        }
+
+        TaskEntity taskEntity = taskRepository.getOne(taskId);
+        if (taskEntity == null) {
+            log.info("There is no such task");
+            throw new Exception("Такой задачи не существует");
+        }
+
+        projectEntity.getTasks().add(taskEntity);
+
+        projectRepository.save(projectEntity);
     }
 
     @Override
+    @SneakyThrows
     public void delTaskToProject(Long projectId, Long taskId) {
         log.info("add task: " + taskId + "to project: " + projectId);
+
+        ProjectEntity projectEntity = projectRepository.getOne(projectId);
+        if (projectEntity == null) {
+            log.info("There is no such project");
+            throw new Exception("Такого проекта не существует");
+        }
+
+        TaskEntity taskEntity = taskRepository.getOne(taskId);
+        if (taskEntity == null) {
+            log.info("There is no such task");
+            throw new Exception("Такой задачи не существует");
+        }
+
+        List<TaskEntity> list = projectEntity.getTasks();
+        list = list.stream().filter(task -> !task.equals(taskEntity)).collect(Collectors.toList());
+
+        projectEntity.setTasks(list);
+
+        projectRepository.save(projectEntity);
+
     }
 }
