5ff891975da1a35cba039ea5041b5c7c2cd3a4be
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-10-25 18:27:37 +0300

    fix(auth): fix configure security

--- src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/ProjectController.java
+++ src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/ProjectController.java
@@ -3,6 +3,7 @@ package com.lyahov.stepa.MyTodoIstBack.web.api.v1;
 import com.lyahov.stepa.MyTodoIstBack.business.service.ProjectService;
 import com.lyahov.stepa.MyTodoIstBack.web.dto.ProjectDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,25 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping("/createProject")
+    @PreAuthorize("hasAuthority('read, write')")
     public void createProject(@RequestBody ProjectDto projectDto) {
         projectService.createProject(projectDto);
     }
 
     @GetMapping("/deleteProject/{id}")
+    @PreAuthorize("hasAuthority('read, write')")
     public void deleteProject(@PathVariable("id") Long id) {
         projectService.deleteProject(id);
     }
 
     @PostMapping("/updateProject")
+    @PreAuthorize("hasAuthority('read, write')")
     public void updateProject(@RequestBody ProjectDto projectDto) {
         projectService.updateProject(projectDto);
     }
 
     @GetMapping("/getAllProject")
+    @PreAuthorize("hasAuthority('read, write')")
     public List<ProjectDto> getAllProject() {
         return projectService.getAllProject();
     }
--- src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/TaskController.java
+++ src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/TaskController.java
@@ -3,6 +3,7 @@ package com.lyahov.stepa.MyTodoIstBack.web.api.v1;
 import com.lyahov.stepa.MyTodoIstBack.business.service.TaskService;
 import com.lyahov.stepa.MyTodoIstBack.web.dto.TaskDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,25 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping("/deleteTask/{id}")
+    @PreAuthorize("hasAuthority('read, write')")
     public void deleteTask(@PathVariable("id") Long id) {
         taskService.deleteTask(id);
     }
 
     @PostMapping("/createTask")
+    @PreAuthorize("hasAuthority('read, write')")
     public void createTask(@RequestBody TaskDto taskDto) {
         taskService.createTask(taskDto);
     }
 
     @PostMapping("/updateTask")
+    @PreAuthorize("hasAuthority('read, write')")
     public void updateTask(@RequestBody TaskDto taskDto) {
         taskService.updateTask(taskDto);
     }
 
     @GetMapping("/getAllTask")
+    @PreAuthorize("hasAuthority('read, write')")
     public List<TaskDto> getAllTask(@RequestParam(required = false) Long projectId) {
         return taskService.getAllTask(projectId);
     }
