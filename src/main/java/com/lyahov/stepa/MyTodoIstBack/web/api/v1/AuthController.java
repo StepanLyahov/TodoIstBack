d0cf4893c79fc4b16d47eaf41ade76abb1ae2f9e
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-10-25 18:12:24 +0300

    fix(auth): fix AuthController

--- src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/AuthController.java
+++ src/main/java/com/lyahov/stepa/MyTodoIstBack/web/api/v1/AuthController.java
@@ -1,24 +1,53 @@
 package com.lyahov.stepa.MyTodoIstBack.web.api.v1;
 
+import com.lyahov.stepa.MyTodoIstBack.business.dao.UserRepository;
+import com.lyahov.stepa.MyTodoIstBack.business.entity.User;
+import com.lyahov.stepa.MyTodoIstBack.security.jwt.JwtTokenProvider;
+import com.lyahov.stepa.MyTodoIstBack.web.dto.AuthRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.lyahov.stepa.MyTodoIstBack.web.ApiConstantUtil.AUTH;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping(AUTH)
 public class AuthController {
 
-    @GetMapping("/login")
-    public String getLoginPage() {
-        return "login";
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody AuthRequestDto request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+            String token = jwtTokenProvider.createToken(request.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", request.getEmail());
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/password combination", HttpStatus.FORBIDDEN);
+        }
     }
 
-    @GetMapping("/success")
-    public String getSuccessPage() {
-        return "success";
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
     }
 }
